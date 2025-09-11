d2f2da45707350dbe7d2ed6855f71b1b8cfb6e0a
jabarreraEH <jabarrera@eaglehubs.com>
2025-09-11 16:24:38 -0600

    Proyecto BE de task-management

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,42 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './tasks.model';
+import { TaskStatus } from './tasks.model';
+import { v4 as uuid } from 'uuid';
+import { Body } from '@nestjs/common';
+import { Param } from '@nestjs/common';
+
+// This controller handles all the calls related to "/tasks" endpoint
+// Notice that what is inside the parenthesis of @Controller is the base route for this controller
+@Controller('tasks')
+export class TasksController {
+    constructor(private tasksService: TasksService) { }
+    private tasks: Task[] = []; // Example tasks
+
+    // This method will handle GET requests to "/tasks"
+    @Get()
+    getAllTasks(): Task[] {
+        return this.tasksService.getAllTasks();
+    }
+
+    @Get('/:id')
+    getTaskById(@Param('id') id: string): Task {
+        return this.tasksService.getTaskById(id);
+    }
+
+    //Con esto, se accede al body de la request
+    // @Post()
+    // createTask(@Body() body) { //: Task {
+    //     console.log(body);
+    // }
+
+    @Post()
+    createTask(
+        @Body('title') title: string,
+        @Body('description') description: string): Task {
+            console.log(title, description);
+            const task = this.tasksService.createTask(title, description);
+            console.log('Task', task.title, 'created:', task);
+            return task;
+    }
+}
